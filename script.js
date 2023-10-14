1045b03a397a4b598860f6f661011da1dcd33da7
stefan g <116798342+stefangoina@users.noreply.github.com>
2023-10-14 23:39:49 +0300

    made the functionality to color the pixels when hover over

--- script.js
+++ script.js
@@ -4,19 +4,6 @@ const squares = document.querySelector(".choose");
 let sqr;
 const container = document.querySelector(".container");
 
-squares.addEventListener("click", () => {
-  sqr = prompt("How Many Squares you Want you Canvas to Have?(1-100)");
-  if (sqr > 100 || sqr < 1) {
-    alert("Invalid Number!");
-    sqr = null;
-  } else {
-    sqr = +sqr;
-  }
-  console.log(`no. of sqrs: ${sqr}`);
-
-  create();
-});
-
 function create() {
   while (sqr) {
     let w = document.createElement("div");
@@ -25,7 +12,36 @@ function create() {
     w.textContent = "";
     sqr--;
   }
+  const pixel = Array.from(document.querySelectorAll(".pixel"));
+  console.log(pixel);
+  let colorOnHover = () => {
+    pixel.map((a) => {
+      a.addEventListener("mouseover", (event) => {
+        event.stopPropagation();
+        let currOpacity = event.target.style.opacity;
+        event.target.style.backgroundColor = "#f4f4f4";
+        if (currOpacity >= 0.2) {
+          event.target.style.opacity = currOpacity - 0.1;
+        }
+      });
+    });
+  };
+  colorOnHover();
 }
+
+let createGrid = () => {
+  squares.addEventListener("click", () => {
+    sqr = prompt("How Many Squares you Want you Canvas to Have?(1-100)");
+    if (sqr > 100 || sqr < 1) {
+      alert("Invalid Number!");
+      sqr = null;
+    } else {
+      sqr = +(sqr * sqr);
+    }
+    console.log(`no. of sqrs: ${sqr}`);
+    create();
+  });
+};
 
 // restart button ( when clicked => refresh page)
 const refreshButton = document.querySelector(".restart");
@@ -34,12 +50,7 @@ refreshButton.addEventListener("click", () => {
 });
 // ----------------------------------------------------------------
 
-// adaugam culoare cand dai hover peste div
-const pixel = document.querySelectorAll(".pixel");
-
-pixel.forEach((item) => {
-  item.addEventListener("click", () => {
-    console.log(this);
-    this.style.backgroundColor = "white";
-  });
-});
+function run() {
+  createGrid();
+}
+run();
